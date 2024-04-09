cda5b593e4d175b917ecb482cf0248b09c04dfbc
Dan Gutiérrez <dany_rocker_96@hotmail.com>
2024-04-08 22:13:49 -0700

    cambios de Inicio

--- app/src/main/java/equipo6/xtremetecpc/xtpc_app/Inicio.kt
+++ app/src/main/java/equipo6/xtremetecpc/xtpc_app/Inicio.kt
@@ -1,4 +1,11 @@
 package equipo6.xtremetecpc.xtpc_app
 
-class Inicio {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Inicio : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio)
+    }
 }
